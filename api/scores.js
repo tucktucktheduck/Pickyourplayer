a35e368dddc30757883583ed7f11e80a9e1459b7
tucktucktheduck <68677416+tucktucktheduck@users.noreply.github.com>
2026-05-30 21:42:54 -0400

    Update scores.js

--- api/scores.js
+++ api/scores.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const r = await fetch('https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard');
+  const gameId = req.query.game;
+  const url = gameId
+    ? `https://site.api.espn.com/apis/site/v2/sports/basketball/nba/${gameId}/boxscore`
+    : `https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard`;
+  const r = await fetch(url);
   const d = await r.json();
   res.json(d);
 }
