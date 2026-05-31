3c5dacb66c71243efa3155d8f22e1d265c4a6ec6
tucktucktheduck <68677416+tucktucktheduck@users.noreply.github.com>
2026-05-30 21:54:41 -0400

    Update scores.js

--- api/scores.js
+++ api/scores.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-store, max-age=0');
   const gameId = req.query.game;
   const url = gameId
     ? `https://site.api.espn.com/apis/site/v2/sports/basketball/nba/${gameId}/boxscore`
